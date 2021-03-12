2f585e8753828a5314bea228c853737d551f03f8
marcel-git <marcel_kuehn@gmx.de>
2021-03-12 22:36:15 +0100

    Initial commit

---
+++ src/test/java/com/ctfservice/eurkaserver/EurkaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctfservice.eurkaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurkaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
